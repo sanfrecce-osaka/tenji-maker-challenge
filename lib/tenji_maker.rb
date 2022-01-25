b39fe4f0b69f89f2b67990b12299ac5609b42524
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-01-25 20:21:53 +0900

    辛口バージョンのテストを通るコードを実装
    cf. https://gist.github.com/JunichiIto/1c8565bbffd0b5cf589b905f9ad2388f

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,43 +1,81 @@
 class TenjiMaker
+  TENJI_MAP = {
+    bo_in: {
+      'A' => '- --',
+      'K' => '- -o',
+      'S' => 'o -o',
+      'T' => 'o o-',
+      'N' => '- o-',
+      'H' => '- oo',
+      'M' => 'o oo',
+      'Y' => '-o -',
+      'R' => 'o --',
+      'W' => '-- -',
+    },
+    shi_in_having_five: {
+      'A' => 'o- -',
+      'I' => 'o- o',
+      'U' => 'oo -',
+      'E' => 'oo o',
+      'O' => '-o o',
+    },
+    shi_in_not_having_five: {
+      'A' => '- o-',
+      'U' => '- oo',
+      'O' => 'o o-',
+    },
+    you_on_with_sei_on: '-o -- --',
+    you_on_with_daku_on: '-o -o --',
+    you_on_with_han_daku_on: '-o -- -o',
+    soku_on: '-- o- --',
+    daku_on: '-- -o --',
+    han_daku_on: '-- -- -o',
+    hatsu_on: '-- -o oo',
+    chou_on: '-- oo --',
+  }
+
+  SEION_MAP = {
+    'G' => 'K',
+    'Z' => 'S',
+    'D' => 'T',
+    'B' => 'H',
+    'P' => 'H',
+  }
+
   def to_tenji(text)
-    # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
-    case text
-    when 'A HI RU'
-      <<~TENJI.chomp
-        o- o- oo
-        -- o- -o
-        -- oo --
-      TENJI
-    when 'KI RI N'
-      <<~TENJI.chomp
-        o- o- --
-        o- oo -o
-        -o -- oo
-      TENJI
-    when 'SI MA U MA'
-      <<~TENJI.chomp
-        o- o- oo o-
-        oo -o -- -o
-        -o oo -- oo
-      TENJI
-    when 'NI WA TO RI'
-      <<~TENJI.chomp
-        o- -- -o o-
-        o- -- oo oo
-        o- o- o- --
-      TENJI
-    when 'HI YO KO'
-      <<~TENJI.chomp
-        o- -o -o
-        o- -o o-
-        oo o- -o
-      TENJI
-    when 'KI TU NE'
-      <<~TENJI.chomp
-        o- oo oo
-        o- -o o-
-        -o o- o-
-      TENJI
+    tenjis_from(text).transpose.map { |column| column.join(' ') }.join("\n")
+  end
+
+  private
+
+  def parse(char)
+    case char.chars
+    in [/[KSTNHMR]/ => sei_on, 'Y', *rests]
+      [TENJI_MAP[:you_on_with_sei_on], parse([sei_on, *rests].join(''))]
+    in [/[GZDB]/ => daku_on, 'Y', *rests]
+      [TENJI_MAP[:you_on_with_daku_on], parse([SEION_MAP[daku_on], *rests].join(''))]
+    in ['P' => han_daku_on, 'Y', *rests]
+      [TENJI_MAP[:you_on_with_han_daku_on], parse([SEION_MAP[han_daku_on], *rests].join(''))]
+    in [/[KSTNHMRWGZDB]/, /[KSTNHMRWGZDB]/ => soku_on, *rests]
+      [TENJI_MAP[:soku_on], parse([soku_on, *rests].join(''))]
+    in [/[KSTNHMR]/ => bo_in, shi_in]
+      TENJI_MAP[:shi_in_having_five][shi_in] + TENJI_MAP[:bo_in][bo_in]
+    in [/[YW]/ => bo_in, shi_in]
+      TENJI_MAP[:bo_in][bo_in] + TENJI_MAP[:shi_in_not_having_five][shi_in]
+    in [/[GZDB]/ => daku_on, *rests]
+      [TENJI_MAP[:daku_on], parse([SEION_MAP[daku_on], *rests].join(''))]
+    in ['P' => han_daku_on, *rests]
+      [TENJI_MAP[:han_daku_on], parse([SEION_MAP[han_daku_on], *rests].join(''))]
+    in ['N']
+      TENJI_MAP[:hatsu_on]
+    in ['-']
+      TENJI_MAP[:chou_on]
+    in [shi_in]
+      TENJI_MAP[:shi_in_having_five][shi_in] + TENJI_MAP[:bo_in]['A']
     end
+  end
+
+  def tenjis_from(text)
+    text.split(' ').map { |char| parse(char) }.flatten.map { |tenji| tenji.split(' ') }
   end
 end
